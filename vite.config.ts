5bd6028447d237fd066bb6890dcc13f4f77ca94e
vaibhav dinkar <vaibhavdinkar2536@gmail.com>
2024-08-14 15:52:09 +0530

    changed vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: Number(process.env.PORT) || 5173, // Convert the PORT env variable to a number
+    host: true, // This makes it accessible from the network
+  },
 })
